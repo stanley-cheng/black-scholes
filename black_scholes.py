39ddcbaf00005307555b7694ed6f235041a9e771
stanley-cheng <69270879+stanley-cheng@users.noreply.github.com>
2022-07-29 18:16:30 +0800

    Add files via upload

--- black_scholes.py
+++ black_scholes.py
@@ -1,5 +1,5 @@
 '''
-This script acts as a calculator for the Black Scholes Implied Volatility, using the Newton Raphson root finding method.
+This module provides functions to calculate the Black Scholes option price, implied volatility and Greeks.
 '''
 
 from scipy.special import ndtr
@@ -10,47 +10,193 @@ import numpy as np
 N = ndtr
 N_prime = norm._pdf
 
-def bs_call(S, K, T, r, vol):
+def bs_price(flag, S, K, T, r, vol):
+    '''
+    Function that calculates the European call or put option price.
+
+    Parameters
+    ----------
+    flag: str
+        'c' or 'p' for call or put option respectively
+    S: float
+        Spot price of the underlying asset
+    K: float
+        Strike price of the option
+    T: float
+        Time to maturity (in years)
+    r: float
+        Risk-free interest rate as a fraction (e.g. 0.01 for 1%)
+    vol: float
+        Black Scholes implied volatility as a fraction 
+    '''
+        
     d1 = (np.log(S / K) + (r + 0.5 * vol * vol) * T) / (vol * np.sqrt(T))
     d2 = d1 - vol * np.sqrt(T)
-    return S * N(d1) - np.exp(-r * T) * K * N(d2)
+    C = S * N(d1) - np.exp(-r * T) * K * N(d2)
 
-def bs_vega(S, K, T, r, vol):
-    d1 = (np.log(S / K) + (r + 0.5 * vol * vol) * T) / (vol * np.sqrt(T))
-    return S * N_prime(d1) * np.sqrt(T)
+    if flag == 'c':
+        return C
+    elif flag == 'p':
+        return C + K * np.exp(-r * T) - S
 
-def find_vol(price, S, K, T, r, *args):
-    ''''
-    Finds Black Scholes implied volatility of a European call option using Newton Raphson root finding method.
+def bs_delta(flag, S, K, T, r, vol):
+    '''
+    Function that calculates the delta of an European call or put option.
 
-    price: float
-    Value of the option
-
+    Parameters
+    ----------
+    flag: str
+        'c' or 'p' for call or put option respectively
     S: float
-    Spot price of the asset
-
+        Spot price of the underlying asset
     K: float
-    Strike price of the option
-
+        Strike price of the option
     T: float
-    Time to maturity in years
-
+        Time to maturity (in years)
     r: float
-    Risk-free interest rate
+        Risk-free interest rate as a fraction (e.g. 0.01 for 1%)
+    vol: float
+        Black Scholes implied volatility as a fraction 
+    '''
+
+    d1 = (np.log(S / K) + (r + 0.5 * vol * vol) * T) / (vol * np.sqrt(T))
+    if flag == 'c':
+        return N(d1)
+    elif flag =='p':
+        return N(d1) - 1
+
+def bs_gamma(flag, S, K, T, r, vol):
+    '''
+    Function that calculates the gamma of an European call or put option.
+
+    Parameters
+    ----------
+    flag: str
+        'c' or 'p' for call or put option respectively
+    S: float
+        Spot price of the underlying asset
+    K: float
+        Strike price of the option
+    T: float
+        Time to maturity (in years)
+    r: float
+        Risk-free interest rate as a fraction (e.g. 0.01 for 1%)
+    vol: float
+        Black Scholes implied volatility as a fraction 
+    '''
+
+    d1 = (np.log(S / K) + (r + 0.5 * vol * vol) * T) / (vol * np.sqrt(T))
+    return (N_prime(d1)) / (S * vol * np.sqrt(T))
+
+def bs_vega(flag, S, K, T, r, vol):
+    '''
+    Function that calculates the vega of an European call or put option, defined as the change in option price for a 1% change in implied volatility.
+
+    Parameters
+    ----------
+    flag: str
+        'c' or 'p' for call or put option respectively
+    S: float
+        Spot price of the underlying asset
+    K: float
+        Strike price of the option
+    T: float
+        Time to maturity (in years)
+    r: float
+        Risk-free interest rate as a fraction (e.g. 0.01 for 1%)
+    vol: float
+        Black Scholes implied volatility as a fraction 
+    '''
+
+    d1 = (np.log(S / K) + (r + 0.5 * vol * vol) * T) / (vol * np.sqrt(T))
+    return S * N_prime(d1) * np.sqrt(T) / 100
+
+def bs_theta(flag, S, K, T, r, vol):
+    '''
+    Function that calculates the theta of an European call or put option.
+
+    Parameters
+    ----------
+    flag: str
+        'c' or 'p' for call or put option respectively
+    S: float
+        Spot price of the underlying asset
+    K: float
+        Strike price of the option
+    T: float
+        Time to maturity (in years)
+    r: float
+        Risk-free interest rate as a fraction (e.g. 0.01 for 1%)
+    vol: float
+        Black Scholes implied volatility as a fraction 
+    '''
+
+    d1 = (np.log(S / K) + (r + 0.5 * vol * vol) * T) / (vol * np.sqrt(T))
+    d2 = d1 - vol * np.sqrt(T)
+    
+    if flag == 'c':
+        return ( -(S * N_prime(d1) * sigma) / (2 * np.sqrt(T)) - r * K * np.exp(-r * T) * N(d2) ) / 365
+    elif flag == 'p':
+        return ( -(S * N_prime(d1) * sigma) / (2 * np.sqrt(T)) + r * K * np.exp(-r * T) * N(-d2) ) / 365
+
+def bs_rho(flag, S, K, T, r, vol):
+    '''
+    Function that calculates the rho of an European call or put option.
+
+    Parameters
+    ----------
+    flag: str
+        'c' or 'p' for call or put option respectively
+    S: float
+        Spot price of the underlying asset
+    K: float
+        Strike price of the option
+    T: float
+        Time to maturity (in years)
+    r: float
+        Risk-free interest rate as a fraction (e.g. 0.01 for 1%)
+    vol: float
+        Black Scholes implied volatility as a fraction 
+    '''
+
+    d1 = (np.log(S / K) + (r + 0.5 * vol * vol) * T) / (vol * np.sqrt(T))
+    d2 = d1 - vol * np.sqrt(T)
+    
+    if flag == 'c':
+        return K * T * np.exp(-r * T) * N(d2) / 100
+    elif flag == 'p':
+        return -K * T * np.exp(-r * T) * N(-d2) / 100
+
+
+def bs_impliedvol(flag, price, S, K, T, r, *args):
+    ''''
+    Finds Black Scholes implied volatility of a European call or put option using Newton Raphson root finding method.
+    
+    Attributes
+    ----------
+    flag: str
+        'c' or 'p' for call or put option respectively
+    price: float
+        Price of the option
+    S: float
+        Spot price of the underlying asset
+    K: float
+        Strike price of the option
+    T: float
+        Time to maturity (in years)
+    r: float
+        Risk-free interest rate as a fraction (e.g. 0.01 for 1%)
     '''
 
     MAX_STEPS = 200
     PRECISION = 1.0e-5 
     sigma = 0.5 # first guess of implied volatility
     for i in range(0, MAX_STEPS):
-        bs_price = bs_call(S, K, T, r, sigma) # bs price
-        vega = bs_vega(S, K, T, r, sigma)
-        diff = price - bs_price
+        P = bs_price(flag, S, K, T, r, sigma) # bs price
+        vega = bs_vega(flag, S, K, T, r, sigma) * 100
+        diff = price - P
         if (abs(diff) < PRECISION):
             return sigma
         sigma = sigma + diff / vega
     return sigma
-
-
-
 
